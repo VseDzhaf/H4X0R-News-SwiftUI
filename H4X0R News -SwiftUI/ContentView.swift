78d491067d4380fb8c858def0db1e1d3f13c42a1
Vsevolod Dzhafarov <sevachok7@gmail.com>
2020-12-06 04:08:52 +0300

    Initial Commit

---
+++ H4X0R News -SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News -SwiftUI
+//
+//  Created by Vsevolod on 06.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
